57541c6d19a587bc2320f900cfd5c65ee0f33e51
huggoparcelly <h.parcelly@gmail.com>
2024-07-01 21:22:50 -0300

    refact badges

--- src/app/components/CategoriesBadges.tsx
+++ src/app/components/CategoriesBadges.tsx
@@ -1,13 +1,13 @@
 export default function CategoriesBadges() {
   return (
     <div className="flex space-x-2 my-3">
-      <span className="inline-flex items-center justify-center flex-grow rounded-md bg-yellow-900 px-3 py-2 text-xs font-medium text-slate-200 ring-1 ring-inset ring-yellow-600/20">
+      <span className="inline-flex items-center justify-center flex-grow rounded-full bg-yellow-900 px-3 py-2 text-xs font-medium text-slate-200 ring-1 ring-inset ring-yellow-600/20">
         Cachorro
       </span>
-      <span className="inline-flex items-center justify-center flex-grow rounded-md bg-yellow-900 px-3 py-2 text-xs font-medium text-slate-200 ring-1 ring-inset ring-yellow-600/20">
+      <span className="inline-flex items-center justify-center flex-grow rounded-full bg-yellow-900 px-3 py-2 text-xs font-medium text-slate-200 ring-1 ring-inset ring-yellow-600/20">
         Gato
       </span>
-      <span className="inline-flex items-center justify-center flex-grow rounded-md bg-yellow-900 px-3 py-2 text-xs font-medium text-slate-200 ring-1 ring-inset ring-yellow-600/20">
+      <span className="inline-flex items-center justify-center flex-grow rounded-full bg-yellow-900 px-3 py-2 text-xs font-medium text-slate-200 ring-1 ring-inset ring-yellow-600/20">
         Pássaro
       </span>
     </div>
